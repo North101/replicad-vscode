8902540167c30a7b379c81d7c5357360acd0b862
North101 <alexander.wilde87@gmail.com>
2025-01-13 05:43:06 +0000

    Center and color Loading view

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -66,7 +66,16 @@ export default function ReplicadApp() {
       </ThreeContext>
     ) : (
       <div
-        style={{ display: 'flex', alignItems: 'center', fontSize: '2em' }}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '2em',
+          width: '100vw',
+          height: '100vh',
+          background: 'var(--vscode-editor-background)',
+          color: 'var(--vscode-editor-foreground)'
+        }}
       >
         Loading...
       </div>
